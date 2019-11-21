81cb8e7b1a14905a9e19f9706673c363a0e9ffe7
btran <btran@Admins-MacBook-Pro-2.local>
2019-11-20 18:03:39 -0800

    Get all products from shopify

---
+++ helpers/restGetProducts.js
@@ -0,0 +1,31 @@
+const apiGetRequest = require("./apiGetRequest");
+
+/**
+ * Query the admin to get all products but limit amount received
+ *
+ * @param   {Number} limit     The amount requested from shopify 
+ * @param   {Number} page      Paginate through Shopify's items on its server 
+ * @returns {Promise<Object[]>} romise object represents the post body
+ */
+
+const getProducts = function(limit = 10, page = 1) {
+    return new Promise(async function(resolve, reject) {
+        const params = {
+            url: `https://${process.env.SHOP}/admin/products.json?limit=${limit}&page=${page}`,
+            headers: {
+              "X-Shopify-Access-Token": process.env.ACCESS_TOKEN,
+              "Content-Type": "application/json",
+            },
+            json: true,
+        };
+        
+        try {
+            const products = await apiGetRequest(params);
+            resolve(products);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = getProducts;
