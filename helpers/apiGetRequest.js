6b4c8e6128082a69014c513eb429030b4d3df10f
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-11-22 10:01:25 -0800

    Update error handling and logging

--- helpers/apiGetRequest.js
+++ helpers/apiGetRequest.js
@@ -12,11 +12,16 @@ const getRequest = function(option) {
     option.json = true;
     request.get(option, function(err, res, body) {
       if (err) {
+        console.log("Request Error");
         reject(err);
+      }
+      if(res.statusCode >= 300) {
+        console.log(res.statusMessage);
+        reject(body);
       }
       if (body && body.hasOwnProperty("error")) {
         reject(body);
-      } 
+      }
       else {
         setTimeout(() => {
           resolve(body);
