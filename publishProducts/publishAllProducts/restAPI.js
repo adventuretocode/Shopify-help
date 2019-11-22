c26989849d883bfcea2a276a6c3e7e373131b19d
btran <btran@Admins-MacBook-Pro-2.local>
2019-11-21 18:55:10 -0800

    Started new mini project to publish everything

--- publishProducts/publishAllProducts/restAPI.js
+++ publishProducts/publishAllProducts/restAPI.js
@@ -2,18 +2,14 @@
  * Make One product live
  */
 
-require("dotenv").config();
-const apiPutRequest = require("../../helpers/apiPutRequest.js");
-
+const getAllProducts = require("../../helpers/getAllProducts.js");
+require("../../config");
 const { SHOP, ACCESS_TOKEN } = process.env;
 
 
-//Get all products
-
-const publishedProduct = function(id) {
+const buildShopifyBody = function(id) {
   return new Promise(async function(resolve, reject) {
     try {
-
       const options = {
         url: `https://${SHOP}.myshopify.com/admin/api/2019-10/products/${id}.json`,
         headers: {
@@ -23,12 +19,12 @@ const publishedProduct = function(id) {
           product: {
             id: id,
             // true to publish and false hide
-            published: true,
+            published: true
           }
         }
       };
 
-      const { product } = await apiPutRequest(options);
+      const { product } = await getProducts(options);
       resolve(product);
     } catch (error) {
       reject(error);
@@ -36,24 +32,66 @@ const publishedProduct = function(id) {
   });
 };
 
-const main = async function(id) {
-  try {
-    const product = getSingleProductInfo(id);
-    const { published_at } = product;
+const processProductsToPublished = function(products) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      for (let i = 0; i < products.length; i+=1) {
+        const { id: productId, title: productTitle , published_at } = products[i];
+        if(!published_at) {
+          const { id, title } = await buildShopifyBody(productId);
+          console.log(`\u001b[38;5;${id % 255}m${title}\u001b[0m`);
+        }
+        console.log("Already Published: ", `\u001b[38;5;${productId % 255}m${productTitle}\u001b[0m`);
+      }
 
-    if (published_at) {
-      return "Already Published";
-    } else {
-      const shopifyProduct = await publishedProduct(id);
-      console.log(shopifyProduct.id);
-      return "Published";
+      resolve("next");
+    } catch (error) {
+      reject(error);
     }
+  });
+}
+
+const main = async function() {
+  try {
+    let pages = 1;
+    let moreItems = true;
+    let debug = true;
+
+    while (moreItems) {
+      const shopifyProducts = await getAllProducts(SHOP, ACCESS_TOKEN, 10, pages);
+
+      if(!shopifyProducts) {
+        console.log("Product is null");
+        continue;
+      }
+
+      const { products } = shopifyProducts;
+
+      if(products.length) {
+        await processProductsToPublished(products);
+        pages += 1;
+      } 
+      else {
+        console.log("No products Found", products);
+        moreItems = false;
+        break;
+      }
+
+      //Debug
+      if(debug) {
+        if(pages > 1) {
+          return;
+        }
+      }
+
+      console.log(`==================== ${pages} ===================`);
+    }
+    return "Completed";
   } catch (error) {
     throw error;
   }
 };
 
-main("4348927246435")
+main()
   .then(data => console.log("Success: ", data))
-  .catch(error => console.log("Errors: ", error));
-
+  .catch(error => console.log("Errors: Main - ", error));
