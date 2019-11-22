0d2cadfec70ec871be1b1d161328d9fc62b74f73
btran <btran@Admins-MacBook-Pro-2.local>
2019-11-21 18:52:54 -0800

    Adjust error to throw on request error
    Body is when shopify throws in error but comes back successful

--- helpers/apiGetRequest.js
+++ helpers/apiGetRequest.js
@@ -1,29 +1,28 @@
 const request = require("request");
 
 /**
- * Get request 
+ * Get request
  *
  * @param   {Object} option The request object for shopify
  * @returns {Promise}       Promise object represents the post body
  */
 
-const getRequest = function (option) {
-
-  return new Promise(function (resolve, reject) {
-      option.json = true;
-      request.get(option, function (err, res, body) {
-          if (err) {
-              reject(err);
-          }
-          if(body.errors) {
-            reject(body);
-          }
-          else {
-            setTimeout(() => {
-              resolve(body);
-            }, 250);
-          }
-      });
+const getRequest = function(option) {
+  return new Promise(function(resolve, reject) {
+    option.json = true;
+    request.get(option, function(err, res, body) {
+      if (err) {
+        reject(err);
+      }
+      if (body && body.hasOwnProperty("error")) {
+        reject(body);
+      } 
+      else {
+        setTimeout(() => {
+          resolve(body);
+        }, 250);
+      }
+    });
   });
 };
 
