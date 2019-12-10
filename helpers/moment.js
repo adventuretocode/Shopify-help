6630f30386418e8b37b9af431564b7f42619bd99
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-09 21:13:49 -0800

    Add moment time zone

--- helpers/moment.js
+++ helpers/moment.js
@@ -1,6 +1,12 @@
 const moment = require("moment");
+const momentTimeZone = require("moment-timezone");
 
 // A set time
-console.log(moment("12-25-1995", "MM-DD-YYYY").format());
+// console.log(moment("12-25-1995", "MM-DD-YYYY").format());
 // Now
-console.log(moment().format());
+// console.log(moment().format());
+
+
+const today = momentTimeZone().tz('America/New_York').format("YYYY-MM-DD");
+const yesterday = moment(today, "YYYY-MM-DD").subtract(1, 'days').format("YYYY-MM-DD");
+console.log(yesterday);
