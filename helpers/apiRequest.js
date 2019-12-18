0c3d9b03c5d04e0d97d56d824b7c6360c2fa28e6
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-17 18:19:47 -0800

    Created generic rest api

---
+++ helpers/apiRequest.js
@@ -0,0 +1,34 @@
+const request = require("request");
+
+/**
+ * Get request
+ *
+ * @param   {Object} option The request object for shopify
+ * @returns {Promise}       Promise object represents the post body
+ */
+
+const getRequest = function(option) {
+  return new Promise(function(resolve, reject) {
+    option.json = true;
+    request(option, function(err, res, body) {
+      if (err) {
+        console.log("Request Error");
+        reject(err);
+      }
+      if(res && res.statusCode >= 300) {
+        console.log(res.statusMessage);
+        reject(body);
+      }
+      if (body && body.hasOwnProperty("error")) {
+        reject(body);
+      }
+      else {
+        setTimeout(() => {
+          resolve(body);
+        }, 250);
+      }
+    });
+  });
+};
+
+module.exports = getRequest;
