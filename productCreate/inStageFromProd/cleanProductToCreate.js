028a9c5eee1aa1fa8b259bf3fa790654e34b1a08
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-17 18:22:26 -0800

    Parsed shopify rest object to create product on shopify

---
+++ productCreate/inStageFromProd/cleanProductToCreate.js
@@ -0,0 +1,204 @@
+/**
+ * Create Product via shopify rest api
+ *
+ * @param  {<Object>}       param               Product object from Rest query
+ * @param  {String}         param.title         Product title
+ * @param  {String}         param.body_html     Product content on pdp
+ * @param  {String}         param.vendor        Product artist
+ * @param  {String}         param.product_type  Product type
+ * @param  {String}         param.handle        Product URL
+ * @param  {Array<String>}  param.tags          Product tags
+ * @param  {Array<Object>}  param.variants      Variants attached to this product
+ * @param  {Array<Object>}  param.options       Options selected such as size
+ * @param  {Array<Object>}  param.images        Images attached to product
+ * @param  {Array<Object>}  param.metafields    metafields of the product
+ * @return {Promise<Object>}
+ */
+
+exports.cleanProductToCreateRest = ({
+  product: {
+    title,
+    body_html,
+    vendor,
+    product_type,
+    handle,
+    tags,
+    variants,
+    options,
+    images,
+    metafields
+  }
+}) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const cleanVariant = variants.map(
+        ({
+          title,
+          price,
+          sku,
+          option1,
+          option2,
+          option3,
+          weight,
+          weight_unit
+        }) => {
+          return {
+            title,
+            price,
+            sku,
+            option1,
+            option2,
+            option3,
+            weight,
+            weight_unit
+          };
+        }
+      );
+
+      const cleanOption = options.map(({ name, values }) => {
+        return { name, values };
+      });
+
+      const cleanImages = images.map(({ src }) => {
+        return { src };
+      });
+
+      const cleanMetafields = metafields.map(
+        ({ key, value, value_type, namespace }) => {
+          return {
+            key,
+            value,
+            value_type,
+            namespace
+          };
+        }
+      );
+
+      const cleanProduct = {
+        product: {
+          title,
+          body_html,
+          vendor,
+          product_type,
+          handle,
+          published: true,
+          template_suffix: "2019-pdp",
+          tags,
+          variants: cleanVariant,
+          options: cleanOption,
+          images: cleanImages,
+          metafields: cleanMetafields
+        }
+      };
+
+      resolve(cleanProduct);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+/**
+ * Create product using graphQL
+ *
+ * @param  {<Object>}       param               Product object from Rest query
+ * @param  {String}         param.title         Product title
+ * @param  {String}         param.body_html     Product content on pdp
+ * @param  {String}         param.vendor        Product artist
+ * @param  {String}         param.product_type  Product type
+ * @param  {String}         param.handle        Product URL
+ * @param  {Array<String>}  param.tags          Product tags
+ * @param  {Array<Object>}  param.variants      Variants attached to this product
+ * @param  {Array<Object>}  param.options       Options selected such as size
+ * @param  {Array<Object>}  param.images        Images attached to product
+ * @param  {Array<Object>}  param.image         Image variant attaches itself too
+ * @param  {Array<Object>}  param.metafields    metafields of the product
+ * @return {Promise<Object>}
+ */
+
+exports.cleanProductToCreateGraphql = ({
+  product: {
+    title,
+    body_html,
+    vendor,
+    product_type,
+    handle,
+    tags,
+    variants,
+    options,
+    images,
+    image
+  }
+}) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const cleanTags = tags.split(", ");
+
+      const cleanOption = options.map(({ name }) => {
+        return name;
+      });
+
+      const cleanImages = images.map(({ src }) => {
+        return { src };
+      });
+
+      const cleanVariant = variants.map(
+        ({
+          option1 = null,
+          option2 = null,
+          option3 = null,
+          price,
+          sku,
+          title,
+          weight
+        }) => {
+          return {
+            imageSrc: image.src,
+            inventoryItem: {
+              tracked: false
+            },
+            inventoryPolicy: "DENY",
+            options: [option1, option2, option3],
+            price,
+            requiresShipping: true,
+            sku,
+            title,
+            weight,
+            weightUnit: "POUNDS"
+          };
+        }
+      );
+
+      const cleanMetafields = metafields.map(
+        ({ key, value, value_type, namespace }) => {
+          return {
+            key,
+            value,
+            valueType: value_type,
+            namespace,
+            description: null
+          };
+        }
+      );
+
+      const cleanProduct = {
+        input: {
+          handle,
+          bodyHtml: body_html,
+          images: cleanImages,
+          options: cleanOption,
+          productType: product_type,
+          tags: cleanTags,
+          templateSuffix: "2019-pdp",
+          title,
+          vendor,
+          variants: cleanVariant,
+          metafields: cleanMetafields
+        }
+      };
+      resolve(cleanProduct);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
