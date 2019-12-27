369e67ba2f9bfe094119eeabb6ab447e075c2576
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-27 09:56:36 -0800

    Change config to seperate rivaltee and teefury

--- config.js
+++ config.js
@@ -1,22 +1,30 @@
 const path = require("path");
-
+const { NODE_ENV, STORE } = process.env;
 let keys = "";
-if (process.env.NODE_ENV === "stage-teefury") {
-  keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.stage-teefury")
-  });
-} else if (process.env.NODE_ENV === "prod-teefury") {
-  keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.prod-teefury")
-  });
-} else if (process.env.NODE_ENV === "stage-rivalTees") {
-  keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.stage-rivalTees")
-  });
-} else if (process.env.NODE_ENV === "prod-rivalTees") {
-  keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.prod-rivalTees")
-  });
+
+if (STORE === "teefury") {
+  if (NODE_ENV === "prod") {
+    keys = require("dotenv").config({
+      path: path.join(__dirname, "./.env.prod-teefury")
+    });
+  }
+  else if (NODE_ENV === "stage") {
+    keys = require("dotenv").config({
+      path: path.join(__dirname, "./.env.stage-teefury")
+    });
+  }
+}
+else if (STORE === "rivaltees") {
+  if (NODE_ENV === "prod") {
+    keys = require("dotenv").config({
+      path: path.join(__dirname, "./.env.prod-rivalTees")
+    });
+  }
+  else if (NODE_ENV === "stage") {
+    keys = require("dotenv").config({
+      path: path.join(__dirname, "./.env.stage-rivalTees")
+    });
+  }
 }
 
 module.exports = keys;
