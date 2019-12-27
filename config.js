8287c4944a73d48a363c05506bc9c9be20e70cb3
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-27 09:38:32 -0800

    Add env files for rivalTees

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
 const path = require("path");
 
 let keys = "";
-if(process.env.NODE_ENV === "stage") {
+if (process.env.NODE_ENV === "stage-teefury") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.stage"),
+    path: path.join(__dirname, "./.env.stage-teefury")
   });
-}
-else if (process.env.NODE_ENV === "prod") {
+} else if (process.env.NODE_ENV === "prod-teefury") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.prod"),
+    path: path.join(__dirname, "./.env.prod-teefury")
+  });
+} else if (process.env.NODE_ENV === "stage-rivalTees") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.stage-rivalTees")
+  });
+} else if (process.env.NODE_ENV === "prod-rivalTees") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.prod-rivalTees")
   });
 }
 
