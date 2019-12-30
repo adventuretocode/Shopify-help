cf0fe98e152ef2b95db178ed825b091aa5e176ad
btran <btran@Admins-MacBook-Pro-2.local>
2019-12-30 12:34:16 -0800

    Add Documentation for function

--- helpers/buildRestBody.js
+++ helpers/buildRestBody.js
@@ -2,7 +2,13 @@ require("../config.js");
 const apiRequest = require("./apiRequest.js");
 const { SHOP, ACCESS_TOKEN } = process.env;
 
-
+/**
+ * Building rest API end point
+ * 
+ * @param  {String} url    the url param of the end point
+ * @param  {String} method HTTP method: GET, POST, PUT, DELETE
+ * @param  {Object} body   The body if need to post data
+ */
 
 const buildRestBody = function(url, method, body) {
   return new Promise(async function(resolve, reject) {
