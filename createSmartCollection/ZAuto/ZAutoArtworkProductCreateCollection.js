9c2bee2c094e0d03cf755aa4a23cfa3113bd3cbe
btran <btran@Admins-MacBook-Pro-2.local>
2020-02-05 09:20:57 -0800

    Reformat file to 2 spaces

--- createSmartCollection/ZAuto/ZAutoArtworkProductCreateCollection.js
+++ createSmartCollection/ZAuto/ZAutoArtworkProductCreateCollection.js
@@ -1,28 +1,28 @@
 /**
  * Creating smart collection with Z tag with title and handle
  * Naming convention artist_artwork
- * 
+ *
  * Must Run ZAutoJsonCreate.js first to to build the ZAuto tags
- * 
- * It will check from `./createdCollection/ZAutoProduct-${NODE_ENV}-${STORE}.json` 
+ *
+ * It will check from `./createdCollection/ZAutoProduct-${NODE_ENV}-${STORE}.json`
  * to see if the collection has already been created
- * 
+ *
  * This fill will create the smart tags on shopify based on
  * the file `ZAutoJsonWithID{process.env.ENV}.json`
- * 
- * On Success it will record down the return JSON 
+ *
+ * On Success it will record down the return JSON
  * in the folder `./artistProductCollection${process.env.ENV}/`
- * 
+ *
  * And Also log into `./createdCollection/ZAutoProduct-${NODE_ENV}-${STORE}.json`
- * 
+ *
  */
 
 require("../../config.js");
 const request = require("request");
-const path = require('path');
+const path = require("path");
 const fsWriteFile = require("../../helpers/fsWriteFile.js");
 const createFileIfNotExist = require("../../helpers/createFileIfNotExist.js");
-const { ACCESS_TOKEN, SHOP, NODE_ENV, STORE  } = process.env;
+const { ACCESS_TOKEN, SHOP, NODE_ENV, STORE } = process.env;
 const createErrorJsonFileName = `./createdCollection/ErrorZAutoProduct-${NODE_ENV}-${STORE}.json`;
 const createSuccessJsonFileName = `./createdCollection/ZAutoProduct-${NODE_ENV}-${STORE}.json`;
 const prepJsonCreate = `./prepJsonCreate/ZAutoJsonWithID-${NODE_ENV}-${STORE}.json`;
@@ -35,37 +35,36 @@ const prepJsonCreate = `./prepJsonCreate/ZAutoJsonWithID-${NODE_ENV}-${STORE}.js
  */
 
 const postShopify = function(option) {
-    return new Promise(function(resolve, reject) {
-        request.post(option, function(err, res, body) {
-            if (err) {
-                reject(err);
-            }
+  return new Promise(function(resolve, reject) {
+    request.post(option, function(err, res, body) {
+      if (err) {
+        reject(err);
+      }
 
-            setTimeout(() => {
-                resolve(body);
-            }, 500);
-        });
+      setTimeout(() => {
+        resolve(body);
+      }, 500);
     });
+  });
 };
 
 /**
- * Record 
+ * Record
  * @param {String} tag The tag that is already created on shopify
  * @param {Number} id  Shopify smart collection ID
  */
 
 const recordTag = function(tag, id) {
-    return new Promise(async function(resolve, reject) {
-        try {
-            const json = require(path.join(__dirname, createSuccessJsonFileName));
-            json[tag] = id;
-            await fsWriteFile(path.join(__dirname, createSuccessJsonFileName), json);
-            resolve();
-        } catch (error) {
-            reject(error);
-        }
-    });
-  
+  return new Promise(async function(resolve, reject) {
+    try {
+      const json = require(path.join(__dirname, createSuccessJsonFileName));
+      json[tag] = id;
+      await fsWriteFile(path.join(__dirname, createSuccessJsonFileName), json);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 /**
@@ -75,16 +74,16 @@ const recordTag = function(tag, id) {
  */
 
 const checkTagExist = function(tag) {
-    return new Promise(function(resolve, reject) {
-        try {
-            const json = require(path.join(__dirname, createSuccessJsonFileName));
-            resolve(json[tag]);
-        } catch (error) {
-            console.log("Error: checkTagExist - ", tag);
-            reject(error);
-        } 
-    });
-}
+  return new Promise(function(resolve, reject) {
+    try {
+      const json = require(path.join(__dirname, createSuccessJsonFileName));
+      resolve(json[tag]);
+    } catch (error) {
+      console.log("Error: checkTagExist - ", tag);
+      reject(error);
+    }
+  });
+};
 
 /**
  * Create Smart Collection
@@ -94,60 +93,71 @@ const checkTagExist = function(tag) {
  */
 
 const createSmartCollection = function(body) {
-    return new Promise(async function(resolve, reject) {
-        const params = {
-            url: `https://${SHOP}.myshopify.com/admin/api/2019-10/smart_collections.json`,
-            headers: {
-                "X-Shopify-Access-Token": ACCESS_TOKEN,
-                "Content-Type": "application/json"
-            },
-            body: body,
-            json: true
-        };
-        
-        try {
-            const smartCollectionResponse = await postShopify(params);
-            resolve(smartCollectionResponse);
-        } catch (error) {
-            console.log("Error: createSmartCollection - ", body.smart_collection.title);
-            reject(error);
-        }
-    });
+  return new Promise(async function(resolve, reject) {
+    const params = {
+      url: `https://${SHOP}.myshopify.com/admin/api/2019-10/smart_collections.json`,
+      headers: {
+        "X-Shopify-Access-Token": ACCESS_TOKEN,
+        "Content-Type": "application/json"
+      },
+      body: body,
+      json: true
+    };
+
+    try {
+      const smartCollectionResponse = await postShopify(params);
+      resolve(smartCollectionResponse);
+    } catch (error) {
+      console.log(
+        "Error: createSmartCollection - ",
+        body.smart_collection.title
+      );
+      reject(error);
+    }
+  });
 };
 
 /**
- * Build the body of the 
+ * Build the body of the
  * @param   {String} tag   Tag on shopify
  * @param   {String} title Title of the smart collection
  * @returns {Number}       The id of the smart collection
  */
 
 const createArtistByZTag = function(tag, title) {
-    return new Promise(async function(resolve, reject) {
-        const postBody = {
-            smart_collection: {
-                title: title,
-                rules: [
-                    {
-                        column: "tag",
-                        relation: "equals",
-                        condition: tag
-                    }
-                ]
-            }
-        };
-    
-        try {
-            const createdSmartCollection  = await createSmartCollection(postBody);
-            const { smart_collection: { handle, id } } = createdSmartCollection;
-            await fsWriteFile(path.join(__dirname, `./individual-${NODE_ENV}-${STORE}/${handle}.json`), createdSmartCollection);
-            await recordTag(tag, id);
-            resolve(id);
-        } catch (error) {
-            console.log(" Error: createArtistByZTag - ", tag);
-            reject(error);
-        }
-    });
+  return new Promise(async function(resolve, reject) {
+    const postBody = {
+      smart_collection: {
+        title: title,
+        rules: [
+          {
+            column: "tag",
+            relation: "equals",
+            condition: tag
+          }
+        ]
+      }
+    };
+
+    try {
+      const createdSmartCollection = await createSmartCollection(postBody);
+      const {
+        smart_collection: { handle, id }
+      } = createdSmartCollection;
+      await fsWriteFile(
+        path.join(
+          __dirname,
+          `./individual-${NODE_ENV}-${STORE}/${handle}.json`
+        ),
+        createdSmartCollection
+      );
+      await recordTag(tag, id);
+      resolve(id);
+    } catch (error) {
+      console.log(" Error: createArtistByZTag - ", tag);
+      reject(error);
+    }
+  });
 };
 
 /**
@@ -157,23 +167,22 @@ const createArtistByZTag = function(tag, title) {
  */
 
 const checkIfExist = function(tag, title) {
-    return new Promise(async function(resolve, reject) {
-        try {
-            let id = "Already Created";
-            const isAlreadyCreated = await checkTagExist(tag);
-            if(isAlreadyCreated) {
-                console.log("Already Created: ", tag);
-            }
-            else {
-                id = await createArtistByZTag(tag, title);
-            }
-            resolve(id);
-        } catch (error) {
-            console.log("Error: checkIfExist - ", tag);
-            reject(error);
-        }
-    });
-}
+  return new Promise(async function(resolve, reject) {
+    try {
+      let id = "Already Created";
+      const isAlreadyCreated = await checkTagExist(tag);
+      if (isAlreadyCreated) {
+        console.log("Already Created: ", tag);
+      } else {
+        id = await createArtistByZTag(tag, title);
+      }
+      resolve(id);
+    } catch (error) {
+      console.log("Error: checkIfExist - ", tag);
+      reject(error);
+    }
+  });
+};
 
 /**
  * Main program to run program
@@ -181,38 +190,48 @@ const checkIfExist = function(tag, title) {
  */
 
 const controller = async function(tagsAndTitle) {
-    return new Promise(async function(resolve, reject) {
-        for(const tag in tagsAndTitle) {
-            try {
-                const id = await checkIfExist(tag, tagsAndTitle[tag].handle);
-                const message = id === "Already Created" 
-                    ? id : `\u001b[38;5;${id % 255}mSuccess: ${tag}\u001b[0m`;
-                console.log(message);
-            } catch (error) {
-                console.log("Error: main - ", error, tag);
-                const errorJson = require(path.join(__dirname, createErrorJsonFileName));
-                errorJson[tag] = error;
-                await fsWriteFile(path.join(__dirname, createErrorJsonFileName), errorJson);
-                reject("ZAutoArtworkProductCreateCollection: Reject");
-                break;
-            }
-        }
-        resolve("ZAutoArtworkProductCreateCollection: success");
-    });
-}
+  return new Promise(async function(resolve, reject) {
+    for (const tag in tagsAndTitle) {
+      try {
+        const id = await checkIfExist(tag, tagsAndTitle[tag].handle);
+        const message =
+          id === "Already Created"
+            ? id
+            : `\u001b[38;5;${id % 255}mSuccess: ${tag}\u001b[0m`;
+        console.log(message);
+      } catch (error) {
+        console.log("Error: main - ", error, tag);
+        const errorJson = require(path.join(
+          __dirname,
+          createErrorJsonFileName
+        ));
+        errorJson[tag] = error;
+        await fsWriteFile(
+          path.join(__dirname, createErrorJsonFileName),
+          errorJson
+        );
+        reject("ZAutoArtworkProductCreateCollection: Reject");
+        break;
+      }
+    }
+    resolve("ZAutoArtworkProductCreateCollection: success");
+  });
+};
 
 const main = function() {
-    return new Promise(async function(resolve, reject) {
-        try {
-            await createFileIfNotExist(path.join(__dirname, createErrorJsonFileName));
-            await createFileIfNotExist(path.join(__dirname, createSuccessJsonFileName));
-            const tagsAndTitle = require(path.join(__dirname, prepJsonCreate));
-            const result = await controller(tagsAndTitle)
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
+  return new Promise(async function(resolve, reject) {
+    try {
+      await createFileIfNotExist(path.join(__dirname, createErrorJsonFileName));
+      await createFileIfNotExist(
+        path.join(__dirname, createSuccessJsonFileName)
+      );
+      const tagsAndTitle = require(path.join(__dirname, prepJsonCreate));
+      const result = await controller(tagsAndTitle);
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
 module.exports = main;
