be38cd1b9495e08c66d703289a92e1509c93587f
btran <btran@Admins-MacBook-Pro-2.local>
2020-02-18 17:07:44 -0800

    Moved redirect into redirect folder

--- redirect/redirectsProductsOfCollections/index.js
+++ redirect/redirectsProductsOfCollections/index.js
@@ -1,40 +1,40 @@
-const buildAxiosBody = require("../../helpers/buildAxiosBody.js");
+const buildAxiosBody = require("../../helpers/buildAxiosQuery.js");
 // Get all products in collection.
 // Find the product that is not published
 // Concat the collection handle with the product
 
-
 const productsGetCollection = function(cursor) {
+  return new Promise(async (resolve, reject) => {
     const query = `
-    query productSearchForODAD($num: Int!, $curser: String) {
-      products(first: $num, after: $curser) {
-        pageInfo {
-          hasNextPage
-        }
-        edges {
-          cursor
-          node {
-            handle
-            publishedAt
-            collections(first: 10) {
-              edges {
-                node {
-                  id
-                  handle
+      query productSearchForODAD($num: Int!, $curser: String) {
+        products(first: $num, after: $curser) {
+          pageInfo {
+            hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              handle
+              publishedAt
+              collections(first: 10) {
+                edges {
+                  node {
+                    id
+                    handle
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-    `;
+      `;
 
     const variables = {
-      "num": 10,
-    }
+      num: 1
+    };
 
-    if(curser) {
+    if (curser) {
       variables.curser = cursor;
     }
 
@@ -44,15 +44,15 @@ const productsGetCollection = function(cursor) {
     } catch (error) {
       reject(error);
     }
-
-}
+  });
+};
 
 const main = function() {
   return new Promise(async function(resolve, reject) {
     try {
-      
+
     } catch (error) {
       reject(error);
     }
   });
-}
+};
