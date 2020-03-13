39e47ede3bc16b7436a6c34d2f1f03a88683627e
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-13 10:25:39 -0700

    Sample paginate headers

---
+++ orders/getOrders/paginateOrders.js
@@ -0,0 +1,7 @@
+
+// Paginate is located on the incoming headers Link key
+
+`<https://teefury-com.myshopify.com/admin/api/2019-10/orders.json?limit=2&page_info=eyJzdGF0dXMiOiJhbnkiLCJjcmVhdGVkX2F0X21pbiI6IjIwMTktMTItMDEgMDA6MDA6MDAgVVRDIiwibGFzdF9pZCI6MjA1NTI1MjYzOTgxMCwibGFzdF92YWx1ZSI6IjIwMjAtMDMtMTAgMTU6NTY6NTYiLCJkaXJlY3Rpb24iOiJuZXh0In0>; rel="next"`
+
+`<https://teefury-com.myshopify.com/admin/api/2019-10/orders.json?limit=2&page_info=eyJkaXJlY3Rpb24iOiJwcmV2Iiwic3RhdHVzIjoiYW55IiwiY3JlYXRlZF9hdF9taW4iOiIyMDE5LTEyLTAxIDAwOjAwOjAwIFVUQyIsImxhc3RfaWQiOjIwNTUyNTA0MTE1ODYsImxhc3RfdmFsdWUiOiIyMDIwLTAzLTEwIDE1OjU0OjQ1In0>; rel="previous", 
+<https://teefury-com.myshopify.com/admin/api/2019-10/orders.json?limit=2&page_info=eyJkaXJlY3Rpb24iOiJuZXh0Iiwic3RhdHVzIjoiYW55IiwiY3JlYXRlZF9hdF9taW4iOiIyMDE5LTEyLTAxIDAwOjAwOjAwIFVUQyIsImxhc3RfaWQiOjIwNTUyNDIzMTc4OTAsImxhc3RfdmFsdWUiOiIyMDIwLTAzLTEwIDE1OjQ3OjM5In0>; rel="next"`
