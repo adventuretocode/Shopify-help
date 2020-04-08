34ade5f31cc9490d0401acb20ed1f0147240cb75
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-08 12:54:53 -0700

    Add comments to build GraphqlQuery

--- helpers/buildGraphqlQuery.js
+++ helpers/buildGraphqlQuery.js
@@ -2,6 +2,15 @@ require("../config.js");
 const { SHOP, ACCESS_TOKEN } = process.env; 
 const axiosRequest = require("./axiosRequest.js");
 
+/**
+ * Post request to shopify
+ *
+ * @param   {String}  query     The request object for shopify
+ * @param   {Object}  variables variables to pass into the query params
+ * @param   {Number}  delay     Amount of time in milliseconds before resolving
+ * @returns {Promise}           Promise object represents the post body
+ */
+
 const buildGraphqlQuery = function(query, variables, delay) {
   return new Promise(async function(resolve, reject) {
     try {
@@ -11,7 +20,7 @@ const buildGraphqlQuery = function(query, variables, delay) {
           "Content-Type": "application/json",
           "X-Shopify-Access-Token": ACCESS_TOKEN
         },
-        method: "post",
+        method: "POST",
         data: {
           query: query,
           variables: variables
