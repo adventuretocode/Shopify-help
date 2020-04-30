1a87388e3be0d309722a6f8fa7b1ecd052ea2187
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-29 20:28:44 -0700

    Add new helps

--- helpers/cleanDataDate.js
+++ helpers/cleanDataDate.js
@@ -2,7 +2,15 @@
 // Convert Shopify date into MariaDb date
 // 2020-02-28T23:09:18Z => 2020-04-28 14:39:26
 const convertDate = function(string) {
-  return string.replace("T", " ").replace("Z", "");;
+  if(string.length > 23) {
+    const shopifyDateTime = string.replace("T", " ").replace("Z", "").split("-");
+    shopifyDateTime.pop();
+    const mariaDBDateTime = shopifyDateTime.join("-");
+    return mariaDBDateTime;
+  }
+  else {
+    return string.replace("T", " ").replace("Z", "");
+  }
 }
 
 module.exports = convertDate;
