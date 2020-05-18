ccfa29dbf234362e9144fa3eca620fa77ac6bee5
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-05-17 18:55:35 -0700

    Product create from one store to another

--- config.js
+++ config.js
@@ -5,45 +5,46 @@ let keys = "";
 if (STORE === "teefury") {
   if (NODE_ENV === "prod") {
     keys = require("dotenv").config({
-      path: path.join(__dirname, "./.env.teefury-production")
+      path: path.join(__dirname, "./.env.teefury-production"),
     });
-  }
-  else if (NODE_ENV === "stage") {
+  } else if (NODE_ENV === "stage") {
     keys = require("dotenv").config({
-      path: path.join(__dirname, "./.env.teefury-stage")
+      path: path.join(__dirname, "./.env.teefury-stage"),
     });
-  }
-  else if (NODE_ENV === "dev") {
+  } else if (NODE_ENV === "dev") {
     keys = require("dotenv").config({
-      path: path.join(__dirname, "./.env.teefury-dev")
+      path: path.join(__dirname, "./.env.teefury-dev"),
     });
   }
-}
-else if (STORE === "rivaltees") {
+} else if (STORE === "rivaltees") {
   if (NODE_ENV === "prod") {
     keys = require("dotenv").config({
-      path: path.join(__dirname, "./.env.rivaltees-production")
+      path: path.join(__dirname, "./.env.rivaltees-production"),
     });
-  }
-  else if (NODE_ENV === "stage") {
+  } else if (NODE_ENV === "stage") {
     keys = require("dotenv").config({
-      path: path.join(__dirname, "./.env.rivaltees-stage")
+      path: path.join(__dirname, "./.env.rivaltees-stage"),
     });
   }
-}
-else if (STORE === "cloudapparel") {
+} else if (STORE === "cloudapparel") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.cloudapparel-production")
+    path: path.join(__dirname, "./.env.cloudapparel-production"),
   });
-}
-else if (STORE === "sculptsweat") {
+} else if (STORE === "sculptsweat") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.sculptsweat-production")
+    path: path.join(__dirname, "./.env.sculptsweat-production"),
   });
-}
-else if (STORE === "miraclebrow") {
+} else if (STORE === "miraclebrow") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.miraclebrow-production")
+    path: path.join(__dirname, "./.env.miraclebrow-production"),
+  });
+} else if (STORE === "fantasticheadbands") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.fantasticheadbands-dev"),
+  });
+} else if (STORE === "mooblio") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.moobilo-production"),
   });
 }
 
