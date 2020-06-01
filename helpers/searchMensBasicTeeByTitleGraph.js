e8a198e3d0506ea76ab0c2e3c6a1dc7ee6d8c4df
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-01 15:50:25 -0700

    Query for products with vendor

--- helpers/searchMensBasicTeeByTitleGraph.js
+++ helpers/searchMensBasicTeeByTitleGraph.js
@@ -1,10 +1,11 @@
 const buildAxiosQuery = require("./buildAxiosQuery");
 /**
- * @param  {String} title Title of the product
- * @return {Object}       The product
+ * @param  {String} param.title  Title of the product
+ * @param  {String} param.vendor Vendor/Artist associated to the art
+ * @return {Object}              The product
  */
 
-const searchMensBasicTeeByTitleGraph = function(title) {
+const searchMensBasicTeeByTitleGraph = function({ title, vendor }) {
   return new Promise(async function(resolve, reject) {
     try {
       const query = `
@@ -24,7 +25,7 @@ const searchMensBasicTeeByTitleGraph = function(title) {
       `;
       const variables = {
         num: 50,
-        query: `title:${title} AND product_type:Tee AND tag:style-basic AND tag:gender-mens`
+        query: `title:'${title}' AND vendor:'${vendor}' AND product_type:Tee AND tag:style-basic AND tag:gender-mens`
       };
 
       const products = await buildAxiosQuery(query, variables);
