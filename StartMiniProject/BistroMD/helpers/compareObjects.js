518c3218a05a9f9c0a8b7061d9acb9e3a7c68b08
Bryan Tran <bryan89tran@yahoo.com>
2022-10-04 00:37:14 -0700

    Skip ID column where it is auto generated

--- StartMiniProject/BistroMD/helpers/compareObjects.js
+++ StartMiniProject/BistroMD/helpers/compareObjects.js
@@ -4,6 +4,9 @@
 // Check to see if the projects are the same
 export default (object1, object2) => {
   for (let key in object1) {
+    if(key == 'id') {
+      continue;
+    }
     if (object1[key] != object2[key]) {
       return false;
     }
