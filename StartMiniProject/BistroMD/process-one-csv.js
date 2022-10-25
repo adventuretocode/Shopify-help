7579ea8bd65788553ccf4ef9deea362d5c1fc960
Bryan Tran <bryan89tran@yahoo.com>
2022-10-24 20:49:44 -0700

    Update single CSV

--- StartMiniProject/BistroMD/process-one-csv.js
+++ StartMiniProject/BistroMD/process-one-csv.js
@@ -1,36 +1,68 @@
 import dotenv from "dotenv";
 import neatCsv from "neat-csv";
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile, access } from "fs/promises";
 
 dotenv.config();
 
-const fileLocation = `./data/customers_salesforce.csv`;
-const trackFile = `./data/track.txt`;
+const DIRECTORY = `~/Document`;
+const FILE_NAME = `export`;
+const TRACK_FILE = `./track.txt`;
 
 const main = async () => {
+  console.time();
   try {
+    try {
+      await access(TRACK_FILE);
+    } catch (error) {
+      await writeFile(new URL(TRACK_FILE, import.meta.url), `0`);
+    }
+
+    let trackFile = await readFile(new URL(TRACK_FILE, import.meta.url), {
+      encoding: "utf8",
+    });
+    let startNum = parseInt(trackFile);
+
+    let fileLocation = `${DIRECTORY}/${FILE_NAME}.csv`;
+
+    try {
+      await access(fileLocation);
+    } catch (error) {
+      throw new Error("File Location Not Found");
+    }
+
     const data = await readFile(new URL(fileLocation, import.meta.url), {
       encoding: "utf8",
     });
     const csvArr = await neatCsv(data);
     let endNum = csvArr.length;
-    let startNum = parseInt(
-      await readFile(new URL(trackFile, import.meta.url), {
-        encoding: "utf8",
-      })
-    );
 
     for (let i = startNum; i < endNum; i++) {
       const rowCSV = csvArr[i];
-			// =========================================================
+      // =========================================================
       console.log(rowCSV);
-			// =========================================================
-      await writeFile(new URL(trackFile, import.meta.url), (i + 1).toString());
+      // =========================================================
+      await writeFile(new URL(TRACK_FILE, import.meta.url), (i + 1).toString());
     }
   } catch (error) {
     console.log("Error: ", error);
+    throw error;
   }
 };
 
-main();
-
+main()
+  .then((success) => {
+    console.log("==========================================");
+    console.log(success);
+    console.log("==========================================");
+    console.timeEnd();
+    console.log("==========================================");
+    process.exit();
+  })
+  .catch((err) => {
+    console.log("==========================================");
+    console.log(err);
+    console.log("==========================================");
+    console.timeEnd();
+    console.log("==========================================");
+    process.exit();
+  });
