adbd7dc17bec34d0d7a765e5825110d0e4c58927
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-10-28 14:10:04 -0700

    Add business day for andrew ubt however might not needed

--- StartMiniProject/BistroMD/helpers/moment.js
+++ StartMiniProject/BistroMD/helpers/moment.js
@@ -33,3 +33,36 @@ export const getAmountOfDaysPassed = (startDate, endDate) => {
   const days = moment(dateTwo).diff(moment(dateOne), "days");
   return days;
 };
+
+const addBusinessDays = (originalDate, numDaysToAdd) => {
+  const date = moment(originalDate, "YYYY-MM-DD");
+
+  const Sunday = 0;
+  const Saturday = 6;
+  let daysRemaining = numDaysToAdd;
+
+  const newDate = date.clone();
+
+  while (daysRemaining > 0) {
+    newDate.add(1, "days");
+    if (newDate.day() !== Sunday && newDate.day() !== Saturday) {
+      daysRemaining--;
+    }
+  }
+
+  const formattedDate = moment(newDate).format("YYYY-MM-DD");
+  return formattedDate;
+};
+
+export const processingDates = (nextChargeDate, shipZoneDay = 1) => {
+  const warehouse_date = nextChargeDate;
+  const date = moment(nextChargeDate, "YYYY-MM-DD");
+  const cut_off_date = date.subtract(1, "day");
+  const ship_date = addBusinessDays(warehouse_date, 1);
+  const arrival_date = addBusinessDays(ship_date, shipZoneDay);
+  return {
+    cut_off_date,
+    ship_date,
+    arrival_date,
+  };
+};
